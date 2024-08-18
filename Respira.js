13a850125cf477ad8baf5dace33cbe5df730297d
Valentín Martínez <123801549+ValenteCreativo@users.noreply.github.com>
2024-08-17 20:00:36 -0500

    Update Respira.js

--- Respira.js
+++ Respira.js
@@ -1,5 +1,8 @@
+//Title: RESPIRA.
+//Sal de la tormenta un momento. Tómate 40 segundos para volver a ti. 
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
   noFill();
   stroke(255);
   strokeWeight(2);
@@ -11,9 +14,10 @@ function draw() {
   let t = millis() / 1000; // Tiempo en segundos
   
   // Función de respiración
-  let breathe = sin(t * PI / 4); // Oscilación lenta para simular respiración
-  let size = map(breathe, -1, 1, 100, 300); // Mapeo de la respiración a un tamaño de círculo
+  let breathe = sin(t * PI / 4); // Oscilación para inhalar y exhalar.
+  let size = map(breathe, -1, 1, 100, 250); // Mapeo para calmar la mente...
   
   // Dibujar el círculo
   ellipse(width / 2, height / 2, size, size);
 }
+//@ValenteCreativo
